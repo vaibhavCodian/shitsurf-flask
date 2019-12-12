7ae756bd359516b2b315709d94e1ba436053afeb
Vaibhav <30vaibhavshukla@gmail.com>
2019-12-12 17:27:01 +0530

    Inital

--- shitnet/__init__.py
+++ shitnet/__init__.py
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = 'a5f21203f2c64b4dcea2fbec31723edd'
 if ENV == 'dev':    
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uushrpjwsxyhol:2683e641bd0159e6aefc7547e5495dd12db0012bfafb19bc48b3b86127702d2f@ec2-54-227-250-19.compute-1.amazonaws.com:5432/deqkuq8n66h1rt'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vmpjgckmtjwgxr:2d14a8c7090990980490e1b43eb9d1e3bb27dda5a17405d3f5b2a917d0fc5bc0@ec2-174-129-254-218.compute-1.amazonaws.com:5432/dsqnofkns97f'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
